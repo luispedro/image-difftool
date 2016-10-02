33044d16a3cdfca7e40fb7aad7209d18ba5406e1
Luis Pedro Coelho <luis@luispedro.org>
2016-10-02 14:08:44 +0200

    ENH resize_to resizes to the exact requested size

--- image-diff.py
+++ image-diff.py
@@ -1,5 +1,6 @@
 from sys import argv
 import mahotas as mh
+import mahotas.resize
 from matplotlib import pyplot as plt
 from matplotlib import style
 style.use('seaborn-white')
@@ -9,7 +10,7 @@ put = mh.imread(argv[2])
 axes[0].imshow(orig)
 axes[1].imshow(put)
 if orig.shape != put.shape:
-    orig = mh.imresize(orig, put.shape)
+    orig = mh.resize.resize_to(orig, put.shape)
 axes[2].imshow((orig != put).any(2))
 fig.show()
 plt.pause(20)
