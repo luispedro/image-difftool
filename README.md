368607c76b2a82e9a4d9b88c5709ece1405c8c2a
Luis Pedro Coelho <luis@luispedro.org>
2020-08-01 13:29:41 +0200

    ENH Simplify interface/internals by doing a single script

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Mostly a hack to get a visual diff using [git difftool](https://git-scm.com/docs
 
 Use as
 
-    git difftool --extcmd PATH-TO-compare-images.sh FILE.svg
+    git difftool --extcmd PATH-TO/image-diff.py FILE.svg
+
+
+You need to have [mahotas](https://github.com/luispedro/mahotas) in your Python
+PATH.
 
 Author: Luis Pedro Coelho <luis@luispedro.org>
--- image-diff.py
+++ image-diff.py
@@ -1,13 +1,31 @@
+#!/usr/bin/env python
+
 from sys import argv
 import numpy as np
 import mahotas as mh
 import mahotas.resize
 from matplotlib import pyplot as plt
 from matplotlib import style
+
+
+def convert_image(f, tfile):
+    if f.endswith('.svg'):
+        import subprocess
+        subprocess.check_call([
+            'inkscape',
+            '--export-png', f,
+            tfile])
+
+    return f
+
+
+orig_fname = convert_image(argv[1], '/tmp/imagediff-1.png')
+put = convert_image(argv[2], '/tmp/imagediff-2.png')
+
 style.use('seaborn-white')
 fig,axes = plt.subplots(1,3)
-orig = mh.imread(argv[1])
-put = mh.imread(argv[2])
+orig = mh.imread(orig_fname)
+put = mh.imread(put_fname)
 axes[0].imshow(orig)
 axes[1].imshow(put)
 if orig.shape != put.shape:
